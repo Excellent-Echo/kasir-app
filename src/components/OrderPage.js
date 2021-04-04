34612a517fce61aa2062f72161b37e89e15569e1
mohammedazez <muhamadaziz047@gmail.com>
2021-04-04 20:47:42 +0700

    Release 1 is done

---
+++ src/components/OrderPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// components
+import FormOrder from "./FormOrder";
+
+function OrderPage() {
+  return (
+    <div>
+      <FormOrder />
+    </div>
+  );
+}
+
+export default OrderPage;
