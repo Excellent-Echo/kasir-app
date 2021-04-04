c3efba8e0248846a9529e0e385cf30de1c0f381c
mohammedazez <muhamadaziz047@gmail.com>
2021-04-04 20:53:15 +0700

    Release 2 is done

--- src/components/OrderPage.js
+++ src/components/OrderPage.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // components
 import FormOrder from "./FormOrder";
 
 function OrderPage() {
+  const [orderItems, setOrderItems] = useState([]);
+
+  useEffect(() => {
+    console.log("ini orderItems", orderItems);
+  }, [orderItems]);
+
+  const handleAddItem = (barang, quantity, harga) => {
+    let orderBarangBaru = [...orderItems, { barang, quantity, harga }];
+    setOrderItems(orderBarangBaru);
+  };
+
   return (
     <div>
-      <FormOrder />
+      <FormOrder handleAddItem={handleAddItem} />
     </div>
   );
 }
